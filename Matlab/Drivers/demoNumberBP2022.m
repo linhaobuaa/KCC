145ec90fe2a25c5e52fd03fd286ffca5d3d2dc54
Hao Lin <linhao.lh@qq.com>
2022-05-20 10:25:32 +0800

    update

--- Matlab/Drivers/demoNumberBP2022.m
+++ Matlab/Drivers/demoNumberBP2022.m
@@ -14,14 +14,14 @@ function demoNumberBP
 
 %----------identify all input arguments----------
 %%%% for breast_w dataset %%%%%
-datafile = 'breast_w';
-subfix = '.dat';
-K = 2;
+% datafile = 'breast_w';
+% subfix = '.dat';
+% K = 2;
 
 %%%% for mm dataset %%%%%
-% datafile = 'mm';
-% subfix = '.mat';
-% K = 2;
+datafile = 'mm';
+subfix = '.mat';
+K = 2;
 
 %%%% for reviews dataset %%%%%
 % datafile = 'reviews';
