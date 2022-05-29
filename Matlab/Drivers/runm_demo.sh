55c56cb4e7e0724a3f922f3b2339a05897d857fa
linhaobuaa <linhao.lh@qq.com>
2022-05-29 11:21:20 +0800

    update

--- Matlab/Drivers/runm_demo.sh
+++ Matlab/Drivers/runm_demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/MATLAB/R2022a/bin/matlab -nodisplay -nosplash -nodesktop -r "run('/mnt/data1/home/linhao/KCC/Matlab/Drivers/demo.m');exit;" | tail -n +11
+/usr/local/MATLAB/R2022a/bin/matlab -nodisplay -nosplash -nodesktop -r "run('demo.m');exit;" | tail -n +11
