889d7030c523fc8f46998346cc86a94290a03f6d
linhaobuaa <linhao.lh@qq.com>
2022-06-09 22:05:38 +0800

    update

--- Matlab/Drivers/baseline_OpenEnsembles_JMLR18/openensemble_la12.py
+++ Matlab/Drivers/baseline_OpenEnsembles_JMLR18/openensemble_la12.py
@@ -36,7 +36,9 @@ def main(dataset):
             if idx == 0:
                 continue
 
-            linedata = line.lstrip().strip().split(" ")
+            linedata = line.strip().replace("   ", " ").replace("  ", " ").split(" ")
+            #print (line.strip().split(" "))
+            #print (linedata)
             for j in range(0, len(linedata)):
                 if j % 2 == 0:
                     rows.append(idx - 1)
