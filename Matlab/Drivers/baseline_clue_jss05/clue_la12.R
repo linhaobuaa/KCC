0b53619149f074ba44cc92873d5a3897c5afc566
Hao Lin <linhao.lh@qq.com>
2022-06-19 10:09:31 +0800

    update

--- Matlab/Drivers/baseline_clue_jss05/clue_la12.R
+++ Matlab/Drivers/baseline_clue_jss05/clue_la12.R
@@ -1,5 +1,6 @@
 require("clue")
 require("slam")
+library(R.matlab)
 
 #print(getwd())
 dataset <- "la12"
@@ -27,10 +28,13 @@ for (i in 1:r)
 }
 
 hens <- cl_ensemble(list = bplist)
-consensus_result <- cl_consensus(hens,method="SE",control = list(k = groundtruth_n_classes, verbose = TRUE))
-hard_consensus_result <- cl_class_ids(consensus_result)
-#View(hard_consensus_result)
-#print(typeof(hard_consensus_result))
+num_experiments <- 10
+for (i in 1:num_experiments)
+{
+  consensus_result <- cl_consensus(hens,method="SE",control = list(k = groundtruth_n_classes, verbose = TRUE))
+  hard_consensus_result <- cl_class_ids(consensus_result)
+  #View(hard_consensus_result)
+  #print(typeof(hard_consensus_result))
 
-library(R.matlab)
-writeMat(paste("~/KCC/Matlab/Drivers/baseline_clue_jss05/clue_",dataset,"_consensusresult.mat",sep=""), consensus=hard_consensus_result)
+  writeMat(paste("~/KCC/Matlab/Drivers/baseline_clue_jss05/clue_",dataset,"_consensusresult_",i,".mat",sep=""), consensus=hard_consensus_result)
+}
