841707ca9fe525a0c73e356e2700ac17d0eaf985
Hao Lin <linhao.lh@qq.com>
2022-06-28 08:37:21 +0800

    update

--- Matlab/Comparison/baseline_LinkCluE_JSS10/runm_LinkCluE_la12_time.sh
+++ Matlab/Comparison/baseline_LinkCluE_JSS10/runm_LinkCluE_la12_time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # for 224
-/usr/local/bin/matlab -nodisplay -nosplash -nodesktop -r "run('compare_LinkCluE_la12_time.m');exit;" | tail -n +11
+# /usr/local/bin/matlab -nodisplay -nosplash -nodesktop -r "run('compare_LinkCluE_la12_time.m');exit;" | tail -n +11
 
 # for lxhalle
-# nice /usr/applic/bin/matlab -nodisplay -nosplash -nodesktop -r "run('compare_LinkCluE_la12_time.m');exit;" | tail -n +11
+nice /usr/applic/bin/matlab -nodisplay -nosplash -nodesktop -r "run('compare_LinkCluE_la12_time.m');exit;" | tail -n +11
