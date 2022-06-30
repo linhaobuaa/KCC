4ceb5eaccfac127c1c695fc1784a59708ad8ad70
linhaobuaa <linhao.lh@qq.com>
2022-06-30 14:09:20 +0800

    update

--- Matlab/Drivers/draw_boxplot_NumberBP.sh
+++ Matlab/Drivers/draw_boxplot_NumberBP.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nice /usr/applic/bin/matlab -nodisplay -nosplash -nodesktop -r "run('draw_boxplot_NumberBP.m');exit;" | tail -n +11
+/usr/local/bin/matlab -nodisplay -nosplash -nodesktop -r "run('draw_boxplot_NumberBP.m');exit;" | tail -n +11
